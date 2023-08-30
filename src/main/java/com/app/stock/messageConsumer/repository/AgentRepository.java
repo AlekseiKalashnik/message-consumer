cc52d55323753d064913342831f4e4c6b5b8da28
AlekseiKalashnik <id50538996@gmail.com>
2023-08-30 21:47:08 +0300

    add agent repository

---
+++ src/main/java/com/app/stock/messageConsumer/repository/AgentRepository.java
@@ -0,0 +1,7 @@
+package com.app.stock.messageConsumer.repository;
+
+import com.app.stock.messageConsumer.entity.Agent;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+public interface AgentRepository extends R2dbcRepository<Agent, String> {
+}
