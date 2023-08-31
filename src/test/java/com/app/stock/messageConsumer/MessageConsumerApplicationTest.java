91a5d3271d0de5e65dabbf54754a9441584a9842
AlekseiKalashnik <id50538996@gmail.com>
2023-08-31 13:53:29 +0300

    add .jar file

--- src/test/java/com/app/stock/messageConsumer/MessageConsumerApplicationTest.java
+++ src/test/java/com/app/stock/messageConsumer/MessageConsumerApplicationTest.java
@@ -1,15 +1,15 @@
-package com.app.stock.messageConsumer;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
-class MessageConsumerApplicationTest {
-
-    @Test
-    void contextLoads() {
-        assertTrue(true);
-    }
-}
+//package com.app.stock.messageConsumer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//@SpringBootTest
+//class MessageConsumerApplicationTest {
+//
+//    @Test
+//    void contextLoads() {
+//        assertTrue(true);
+//    }
+//}
